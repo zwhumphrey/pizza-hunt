3efcbdd857e1647e24815a4fa8a156422c82c9f4
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-08-23 18:37:58 -0500

    added controller file

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,7 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  // the functions will go in here as methods
+};
+
+module.exports = pizzaController;
--- models/pizza.js
+++ models/pizza.js
@@ -17,3 +17,9 @@ const PizzaSchema = new Schema({
   },
   toppings: [],
 });
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
