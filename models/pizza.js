9611a5888012b237de890f5949784b7396febcc3
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-08-23 18:03:26 -0500

    added pizza schema for saved data

---
+++ models/pizza.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  size: {
+    type: String,
+    default: 'Large',
+  },
+  toppings: [],
+});
